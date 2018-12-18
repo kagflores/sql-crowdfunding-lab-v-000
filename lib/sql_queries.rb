ef9c2a9a658dc04a599c7580ff9432865c9340cf
Karen Flores <karen.ag.flores@gmail.com>
2018-12-18 04:55:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,6 +41,6 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "SELECT projects.category, SUM(pledges.amount) FROM pledges
 JOIN projects ON projects.id = pledges.project_id
-GROUP BY projects.category = 'books'
-ORDER BY projects.category ASC LIMIT 1"
+WHERE projects.category = 'books'
+GROUP BY projects.category"
 end
